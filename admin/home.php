bca1528e40036cc8624ecd627ea292a710a23a68
felixongom <felixongom2018@gmail.com>
2022-06-02 17:30:57 +0300

    changes applied recentl....y

--- admin/home.php
+++ admin/home.php
@@ -15,8 +15,8 @@
                         <br>
                         <div class="row">
                             <div class="col-xl-3 col-md-6">
-                                <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body">Admins</div>
+                                <div class="card text-white mb-4">
+                                    <div class="card-body">Adminshhhhhh</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
                                         <a class="small text-white stretched-link" href="table_admin.php" style="text-decoration:none">View Details</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
