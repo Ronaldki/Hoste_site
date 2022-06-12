65736dc820c53fe5aed9d4c3066b3a236cd40a30
felixongom <felixongom2018@gmail.com>
2022-06-12 10:22:32 +0300

    new

--- user/js/index.js
+++ user/js/index.js
@@ -13,7 +13,10 @@ $(document).ready(function(){
                 items:2
             },
             1000:{
-                items:4
+                items:3
+            },
+            1400:{
+                items:3
             }
         }
     })
