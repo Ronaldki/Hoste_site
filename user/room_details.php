d94cd72fdf183eacc53faa6084bdb37f9eadc755
felixongom <felixongom2018@gmail.com>
2022-06-20 17:38:58 +0300

    hhh

--- user/room_details.php
+++ user/room_details.php
@@ -3,74 +3,37 @@ include "../include/user_nav_bar.php";
 include "../admin/config/connect.php";
 
 ?>
-
-
-<section class=" hostel_body">
-    <div class="header_div">
-        <h3><?php  echo $_GET['hostel_name']?></h3>
-        <hr id="small_hr">
-
-    </div>
-    <div class="hostel_detail_container">
-
-        <div class="hostel_image">
-            <?php
-            $hostel_id =$_GET['hostel_id'];
-            $status =$_GET['image_status'];
-            $sql ="SELECT * FROM image_tbl WHERE hostel_id = '$hostel_id' AND status = '$status' LIMIT 5";
-            $result =mysqli_query($con, $sql);
-            $big_image = mysqli_fetch_assoc($result);
-            ?>
-            <img src="../admin/upload/<?php echo trim($big_image['image_name']); ?>" alt="" id="large_images">
-            <div class="image_wrapper owl-carousel owl-theme">
-               
-            </div>
-        </div>
-
-        <div class="hostel_details ">
-            
-            <div class="hostel_crdentials">
-                <!-- <p>prize: <span>ugx. 300000</span></p> -->
-                <div class="text-center">
-                    
-                </div>
-                <div class="detail_wrapper">
-                    <div class="table_head">Room Name</div>
-                    <p class="detail_value ">new york</p>
-                </div>
-                <div class="detail_wrapper">
-                    <div class="table_head">Room size</div>
-                    <p class="detail_value ">single</p>
-                </div>
-                <div class="detail_wrapper">
-                    <div class="table_head ">Prize: </div>
-                    <div class="detail_value">Ugx. 300000</div>
-                </div>
-                <div class="detail_wrapper">
-                    <div class="table_head">Status: </div>
-                    <p class="detail_value">not booked</p>
-                </div>
-                <div class="detail_wrapper describe">descrptions</div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet commodi nostrum dolores facere, aspernatur reprehenderit deserunt suscipit nam tempora
-                     molestias natus beatae numquam culpa a optio, consectetur doloribus possimus quos repellat ea voluptatum?</p>
-
-                     <form class="btn-container" action="book.php">
-                         <button class="btn btn-primary w-25">Book</button>
-                     </div>
-            </div>
-
-        </div>
-    </div>
-    <hr>
-
-   
-
-
-<!-- footer -->
 <?php
-include "../include/user_footer.php";
+// $hostel_id = $_GET['hostel_id'];
+// $sql1 = "SELECT * FROM user_tbl JOIN hostel_tbl ON user_tbl.user_id = hostel_tbl.user_id WHERE hostel_tbl.hostel_id = '$hostel_id'";
+// $result = mysqli_query($con, $sql1);
+// $row = mysqli_fetch_assoc($result);
+
+// //   conting the number of rooms fom the hostel
+// $sql2 = "SELECT * FROM room_tbl WHERE hostel_id = $hostel_id";
+// $result2 = mysqli_query($con, $sql2);
+// if ($result2) {
+//     $cont = mysqli_num_rows($result2);
+//     //   echo $cont;
+// } else {
+//     echo mysqli_error($con);
+// }
+
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
 
 
 
@@ -144,7 +107,7 @@ include "../include/user_footer.php";
                                     <!-- <button type="button" class="btn btn-danger btn-close" data-bs-dismiss="modal" aria-label="Close">X</button> -->
                                 </div>
                                 <div class="modal-body">
-                                    <form>
+                                    <form method="POST">
                                         <div class="mb-3">
                                             <label for="recipient-name" class="col-form-label">Room Name...</label>
                                             <!-- <input type="text" class="form-control" id="recipient-name"> -->
@@ -210,3 +173,6 @@ include "../include/user_footer.php";
         </script>
 </body>
 <script src="./js/room_deetail.js"></script>
+
+
+</html>
