ac2966154a2e996d216a3fa79f75433f81cfab4a
Ronaldki <kiberuronaldproff509@gmail.com>
2022-06-20 22:49:09 +0300

    Booking Done

---
+++ user/config/__booking.php
@@ -0,0 +1,58 @@
+<?php
+if (isset($_POST['sub'])) {
+    if (!isset($_SESSION['login_id'])) {
+        echo '<h6 class = "text-danger">Please Login First</h6>';
+    } else {
+        $user_id = $_SESSION['login_id'];
+        $room_id =  $ex_results['room_id'];
+        $hostel_id = $_GET['hostel_id'];
+        $fees  = $_POST['fees'];
+        $book_status = "pending";
+
+        //   Check if Amount is above the minimum amount
+        if($fees < 50000){
+            echo '<h6 class = "text-danger">You can only Book with minimum of ugx: 50000</h6>';
+        }else{
+            //Cehck if room is not booked..............
+            $qy = "SELECT * FROM booking_tbl WHERE room_id =  '$room_id' AND Booking_status ='pending'";
+                $rs = mysqli_query($con,$qy);
+                
+                if(mysqli_num_rows($rs)==0){
+                    
+                    $qry5 = "INSERT INTO booking_tbl (user_id, room_id, hostel_id, fee_paid, Booking_status) VALUES ( '$user_id', '$room_id', '$hostel_id', '$fees','$book_status')";
+        
+                    $qr = mysqli_query($con, $qry5);
+                    
+                    echo '<h6 class = "text-success">Yourhave Successfully Booked '.$ex_results["room_name"].' At a fee of Ugx: '. $_POST["fees"].'</h6>';
+
+                }
+
+        }
+
+    }
+
+}
+
+// disable booking if room is booked...............
+
+$room_id =  $ex_results['room_id'];
+$book_status = "pending";
+$hostel_id = $_GET['hostel_id'];
+
+
+$book = "SELECT * FROM booking_tbl WHERE room_id =  '$room_id' ";
+$bk_data = mysqli_query($con,$book);
+if(mysqli_num_rows($bk_data) ==0){
+
+    echo '<button class="btn btn-primary w-25" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@getbootstrap">Book</button>';
+
+}else{
+     echo '<button class="btn btn-secondary w-25" >Booked</button>';
+
+ 
+
+}
+
+
+
+
--- user/hostel_detail.php
+++ user/hostel_detail.php
@@ -2,8 +2,7 @@
 include "../include/user_nav_bar.php";
 include "../admin/config/connect.php";
 
-?>
-<?php
+
 $hostel_id = $_GET['hostel_id'];
 $sql1 = "SELECT * FROM user_tbl JOIN hostel_tbl ON user_tbl.user_id = hostel_tbl.user_id WHERE hostel_tbl.hostel_id = '$hostel_id'";
 $result = mysqli_query($con, $sql1);
@@ -32,7 +31,7 @@ if ($result2) {
     <div class="hostel_detail_container">
 
         <div class="hostel_image">
-            <img src="../admin/uploads/<?php echo trim($row['hostel_image']); ?>" alt="">
+            <img src="../admin/upload/<?php echo trim($row['hostel_image']); ?>" alt="">
         </div>
 
         <div class="hostel_details ">
@@ -42,7 +41,7 @@ if ($result2) {
                     echo "<i class ='fa fa-user fa-5x text-secondary'></i>";
                 } else { ?>
 
-                    <img src="../admin/uploads/<?php echo trim($row['image']); ?>" alt="">
+                    <img src="../admin/upload/<?php echo trim($row['image']); ?>" alt="">
 
                 <?php
 
@@ -53,13 +52,13 @@ if ($result2) {
                 <button class="btn back text-light bg-primary py-2  px-3">write to <?php echo $row['fname'] . " " . $row['lname']; ?></button>
 
             </div>
-             
-           
+
+
 
             <div class="hostel_crdentials">
                 <!-- <p>prize: <span>ugx. 300000</span></p> -->
                 <div class="text-center">
-                    
+
                 </div>
                 <div class="detail_wrapper">
                     <div class="table_head">No. of rooms: </div>
@@ -88,34 +87,65 @@ if ($result2) {
         <hr>
     </div>
     <div class="roomsavailable">
-<!-- getting and displaying fro the database -->
+        <!-- getting and displaying fro the database -->
         <?php
         $sql1 = "SELECT * FROM room_tbl JOIN  image_tbl ON room_tbl.hostel_id = image_tbl.hostel_id WHERE 
                 image_tbl.hostel_id = '$hostel_id' GROUP BY room_id";
         $result = mysqli_query($con, $sql1);
         // $row = mysqli_fetch_all($result, MYSQLI_ASSOC); 
-        if (mysqli_num_rows($result)==0){
+        if (mysqli_num_rows($result) == 0) {
             echo "<div>No rooma available yet</div>";
-        }else{
-            while($row = mysqli_fetch_assoc($result)){?>
-            
-            <div class="card">
-                <img src="../admin/uploads/<?php echo trim($row['image_name']);?>" width="150px" height="150px" alt="">
-                <div class="card_datails">
-                    <div class="h5 name text-uppercase"><?php echo trim($row['room_name']);?></div>
-                    <div class="status"><?php echo trim($row['room_status']);?></div>
-                    <div class="prize">Ugx: <?php echo trim($row['room_fee']);?></div>
-                    <div class="action">
-                        <button id="book_btn"><a href="">Book</a></button>
-                        <button id="details_btn"><a href="room_details.php">View Detail...</a></button>
+        } else {
+            while ($row = mysqli_fetch_assoc($result)) { ?>
+
+                <div class="card">
+                    <?php
+                    // selecting single and duoble rom image from db
+                    if ($row['room_status'] == "Single") {
+                        $sqlImage = "SELECT * FROM image_tbl WHERE  hostel_id = '$hostel_id' AND status = 'Single'";
+                        $sqlImageresult = mysqli_query($con, $sqlImage);
+                        $imageRow = mysqli_fetch_assoc($sqlImageresult);
+                        // if the ro is avaolable
+                        if ($imageRow) { ?>
+                            <img src="../admin/upload/<?php echo trim($imageRow['image_name']); ?>" width="150px" height="150px" alt="">
+                        <?php
+                        } else { ?>
+                            <img src="../admin/upload/<?php echo trim($row['image_name']); ?>" width="150px" height="150px" alt="">
+                        <?php
+                        }
+
+                        // checkin for double room
+                    } else if ($row['room_status'] == "Double") {
+                        $sqlImages = "SELECT * FROM image_tbl WHERE  hostel_id = '$hostel_id' AND status = 'Double'";
+                        $sqlImageresults = mysqli_query($con, $sqlImages);
+                        $imageRows = mysqli_fetch_assoc($sqlImageresults);
+                        // if the ro is avaolable
+                        if ($imageRows) { ?>
+                            <img src="../admin/uploads/<?php echo trim($imageRows['image_name']); ?>" width="150px" height="150px" alt="">
+                        <?php
+                        } else { ?>
+                            <img src="../admin/uploads/<?php echo trim($row['image_name']); ?>" width="150px" height="150px" alt="">
+                    <?php
+                        }
+                    }
+                    ?>
+
+                    <div class="card_datails">
+                        <div class="h5 name text-uppercase"><?php echo trim($row['room_name']); ?></div>
+                        <div class="status"><?php echo trim($row['room_status']); ?></div>
+                        <div class="prize">Ugx: <?php echo trim($row['room_fee']); ?></div>
+                        <div class="action">
+                            <button id="book_btn"><a href="">Book</a></button> &nbsp; &nbsp;
+                            <button id="details_btn"><a href="room_details.php?hostel_id=<?php echo trim($row["hostel_id"])?>&room_id=<?php echo trim($row["room_id"])?>&image_status=<?php echo trim($row["status"])?>">View Detail...</a></button>
+                            <!--  -->
+                        </div>
                     </div>
+
                 </div>
-    
-            </div>
-            <?php
+        <?php
             }
-        }                   
-        
+        }
+
         ?>
 
 
